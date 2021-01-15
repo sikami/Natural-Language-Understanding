e1146a972c9c069a8f1f7644c591a5ba02dbf892
sikami <widyasari.listya@gmail.com>
2021-01-15 12:55:20 +0000

    added Gui class

---
+++ src/main/java/Gui.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public BorderPane gui() {
+        BorderPane borderPane = new BorderPane();
+
+        return borderPane;
+    }
+}
