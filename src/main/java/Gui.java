abf7ba52e90d740390348194f6894fff669f5819
sikami <widyasari.listya@gmail.com>
2021-01-15 14:44:46 +0000

    changed gui to borderpane that takes hbox and vbox

--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,12 +1,15 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javax.swing.text.LabelView;
-import java.awt.*;
 
 public class Gui extends Application {
 
@@ -17,11 +20,29 @@ public class Gui extends Application {
     }
 
     public BorderPane gui() {
-        BorderPane borderPane = new BorderPane();
-        Label title = new Label("Natural Language Understanding");
-        title.setStyle("-fx-font-weight: bold");
-        title.setFont(new Font("Arial", 16));
-        borderPane.setTop(title);
+        //title
+       BorderPane borderPane = new BorderPane();
+       HBox horizontal = new HBox();
+       horizontal.setSpacing(10);
+       horizontal.setPadding(new Insets(10));
+       VBox vertical = new VBox();
+       vertical.setSpacing(10);
+       vertical.setPadding(new Insets(10));
+       Label title = new Label("Natural Language Understanding");
+       title.setFont(Font.font("Arial",FontWeight.BOLD, 15));
+       horizontal.getChildren().add(title);
+       horizontal.setAlignment(Pos.CENTER);
+       vertical.getChildren().add(horizontal);
+       vertical.getChildren().add(new Separator());
+
+
+
+
+
+
+
+
+        borderPane.setTop(vertical);
         return borderPane;
     }
 }
