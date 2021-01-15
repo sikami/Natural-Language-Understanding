f32492381cf7d668ce4b79540fd9d1423b9c402c
sikami <widyasari.listya@gmail.com>
2021-01-15 12:07:19 +0000

    added a File class

---
+++ src/main/java/File.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class File {
+    private String filePath;
+    private Text textDocument;
+
+    public File(String filePath) {
+        this.filePath = filePath;
+        this.textDocument = new Text("");
+    }
+
+    private void readFiles() {
+
+    }
+}
