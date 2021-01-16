bae0bf6c16e89b8cd7071e656ffae2c7d0245bb2
sikami <widyasari.listya@gmail.com>
2021-01-16 10:33:36 +0000

    added class PasswordReader to read config file

---
+++ src/main/java/PasswordReader.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class PasswordReader {
+
+    private String url;
+    private String mailboxApi;
+    private String gmailUsername;
+    private String gmailPassword;
+    private String ibmApi;
+    private String ibmUrl;
+    private final String CONFIG = "config";
+
+    public PasswordReader(String url) {
+        this.url = url;
+        this.mailboxApi = "";
+        this.gmailPassword = "";
+        this.gmailUsername = "";
+        this.ibmApi = "";
+        this.ibmUrl = "";
+    }
+
+    private String readFile() {
+        //open file
+        //read file one line
+        // convert line into string[]
+        //split by " "
+        //if contains keyword then update constructor with string[1]
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(CONFIG));
+
+        } catch (Exception e) {
+            System.out.println("Config file doesn't exist. Please create one.");
+        }
+        return null;
+    }
+
+}
