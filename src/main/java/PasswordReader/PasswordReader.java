9eaf7e7a11c4a091b32628dd261f7fb35db1c056
sikami <widyasari.listya@gmail.com>
2021-01-18 12:15:39 +0000

    added comment

--- src/main/java/PasswordReader/PasswordReader.java
+++ src/main/java/PasswordReader/PasswordReader.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 
 public class PasswordReader {
 
-    private String pathName;
+    private String pathName; //need to be added later
     private String mailboxApi;
     private String gmailUsername;
     private String gmailPassword;
