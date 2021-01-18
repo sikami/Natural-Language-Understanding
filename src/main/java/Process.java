4aa4cb2950610ae84401388ae11bdf6780e79891
sikami <widyasari.listya@gmail.com>
2021-01-18 11:39:34 +0000

    added connectToWatson method in Process class. So far the method returns void. However this will be changed as soon as I figure out the format of the AnalysisResults return.

--- src/main/java/Process.java
+++ src/main/java/Process.java
@@ -1,3 +1,13 @@
+import PasswordReader.PasswordReader;
+import com.ibm.cloud.sdk.core.security.IamAuthenticator;
+import com.ibm.watson.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.natural_language_understanding.v1.model.EmotionOptions;
+import com.ibm.watson.natural_language_understanding.v1.model.Features;
+
+import java.util.List;
+
 /**
  * Process class.
  * Class where all processing is happening. This includes connection to Watson, sending email via Gmail API, etc.
@@ -7,6 +17,7 @@ public class Process {
     private Text texts;
     private DestinationEmail destinationEmail;
     private KeyPhrase keyPhrase;
+    private PasswordReader passwordReader;
 
     public Process(Text texts, DestinationEmail destinationEmail, KeyPhrase keyPhrase) {
         this.texts = texts;
@@ -18,4 +29,22 @@ public class Process {
         return texts;
     }
 
+    public void connectToWatson() {
+        IamAuthenticator authenticator = new IamAuthenticator(passwordReader.getIbmApi());
+        NaturalLanguageUnderstanding naturalLanguageUnderstanding = new NaturalLanguageUnderstanding("2020-18-01", authenticator);
+        naturalLanguageUnderstanding.setServiceUrl(passwordReader.getIbmUrl());
+
+        String textArticle = this.texts.getText();
+
+        List<String> targets = keyPhrase.getKeys();
+
+        EmotionOptions emotionOptions = new EmotionOptions.Builder().targets(targets).build();
+
+        Features features = new Features.Builder().emotion(emotionOptions).build();
+
+        AnalyzeOptions parameter = new AnalyzeOptions.Builder().text(textArticle).features(features).build();
+
+        AnalysisResults response = naturalLanguageUnderstanding.analyze(parameter).execute().getResult();
+        System.out.println(response);
+    }
 }
