dc44159f27c0adc5e2cfbf80f7ac87764eb4efd0
sikami <widyasari.listya@gmail.com>
2021-01-19 12:41:24 +0000

    change the data type of the emotions to double as its the correct data type

--- src/main/java/Result/Emotion.java
+++ src/main/java/Result/Emotion.java
@@ -2,13 +2,13 @@ package Result;
 
 public class Emotion {
     private String text;
-    private String sadness;
-    private String joy;
-    private String fear;
-    private String disgust;
-    private String anger;
+    private double sadness;
+    private double joy;
+    private double fear;
+    private double disgust;
+    private double anger;
 
-    public Emotion(String text, String sadness, String joy, String fear, String disgust, String anger) {
+    public Emotion(String text, double sadness, double joy, double fear, double disgust, double anger) {
         this.text = text;
         this.sadness = sadness;
         this.joy = joy;
