4effd93cef14ee143d84f0656c20a4e66860618c
sikami <widyasari.listya@gmail.com>
2021-01-19 12:27:39 +0000

    added Emotion class to contain each emotion result

---
+++ src/main/java/Result/Emotion.java
@@ -0,0 +1,19 @@
+package Result;
+
+public class Emotion {
+    private String text;
+    private String sadness;
+    private String joy;
+    private String fear;
+    private String disgust;
+    private String anger;
+
+    public Emotion(String text, String sadness, String joy, String fear, String disgust, String anger) {
+        this.text = text;
+        this.sadness = sadness;
+        this.joy = joy;
+        this.fear = fear;
+        this.disgust = disgust;
+        this.anger = anger;
+    }
+}
